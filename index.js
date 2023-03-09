0ddf287c67845ab39060676abf77123d6f9de9f2
Mohamed Kassoum <82602657+mkassoum@users.noreply.github.com>
2023-03-09 16:38:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,68 @@
 let characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
     "/"];
 
-let specialCha = false
-let noNums = false
-let password = []
+let specialCha = false;
+let noNums = false;
+let password = [];
 
 function generateBtn() {
-    password = []
-    setArray()
+    password = [];
+    setArray();
     for (let x = 0; x < 2; x++) {
         let pass = "";
         for (let i = 0; i < 15; i++) {
-            let n = characters[Math.floor(Math.random() * characters.length)]
-            pass += n
+            let n = characters[Math.floor(Math.random() * characters.length)];
+            pass += n;
         }
-        password.push(pass)
+        password.push(pass);
         if (x === 0) {
-            document.getElementById("password-a").innerHTML = password[x]
-        } else 
-        { document.getElementById("passowrd-b").innerHTML = password[x] }
+            document.getElementById("password-a").innerHTML = password[x];
+        } else if (x === 1) {
+            document.getElementById("password-b").innerHTML = password[x];
+        }
     }
-
 }
 
 function setArray() {
     if (noNums === true && specialCha === true) {
-        characters.splice(52, 39)
+        characters.splice(52, 39);
     } else if (noNums === true) {
-        characters.splice(52, 10)
+        characters.splice(52, 10);
     } else if (specialCha === true) {
-        characters.splice(62, 29)
+        characters.splice(62, 29);
     } else {
-        characters = characters
+        characters = characters;
     }
 }
+
 function switchSpecials() {
-    password = []
-    generateBtn()
+    password = [];
+    generateBtn();
     if (specialCha === false) {
-        specialCha = true
+        specialCha = true;
     } else if (specialCha === true) {
-        specialCha = false
+        specialCha = false;
     }
-    generateBtn()
 }
 
 function switchNumbers() {
-    password = []
+    password = [];
     if (noNums === false) {
-        noNums = true
+        noNums = true;
     } else if (noNums === true) {
-        noNums = false
+        noNums = false;
     }
-    generateBtn()
+    generateBtn();
 }
-function copy() {
 
-    navigator.clipboard.writeText(document.getElementById("password-a").innerText)
-
+function copyPasswordA() {
+    let passwordA = document.getElementById("password-a").innerHTML;
+    navigator.clipboard.writeText(passwordA);
+    alert("Password A copied to clipboard!");
 }
-function copyS() {
-    navigator.clipboard.writeText(document.getElementById("password-b").innerText)
+
+function copyPasswordB() {
+    let passwordB = document.getElementById("password-b").innerHTML;
+    navigator.clipboard.writeText(passwordB);
+    alert("Password B copied to clipboard!");
 }
